390abc61c44570238db3fc6cd59882735e2d91fa
messi18122022 <messi_18122022@protonmail.com>
2024-11-11 20:56:32 +0100

    Initial Commit

---
+++ SciPlot/SciPlotApp.swift
@@ -0,0 +1,17 @@
+//
+//  SciPlotApp.swift
+//  SciPlot
+//
+//  Created by Musa Moin on 11.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct SciPlotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
